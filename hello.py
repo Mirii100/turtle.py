22d9c95ed9d548597639592bdaf96ac17a2e7ea2
mirii Alex Njuguna <145835776+Mirii100@users.noreply.github.com>
2024-01-17 07:50:25 +0300

    Add files via upload
    second commit

---
+++ hello.py
@@ -0,0 +1,25 @@
+print('hello world')
+#prints is used to output  to the screen
+#input is used to take user
+name=input('enter your name  '  )
+print('hello  '+name+'')
+print('*'*10)
+price=100
+print(price)
+#every time inpu is inserted it produces a string and you must cast it to change to interger
+year=input('enter year you were born')
+year1=input('enter the current year')
+#this is casting elements
+age=int(year1)-int(year)
+print(name+'was born in '+year +' and is  ' + str(age) + '  age')
+#python allows tripple " or '
+multline_string='''hae ,
+Alex 
+thanks for starting to practise 
+python code '''
+print(multline_string)
+
+
+
+
+
